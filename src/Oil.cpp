6987901f7da14efa79d97b5efd9f59826fc7a623
Paulchch <pablitombcr@gmail.com>
2022-05-16 20:49:40 -0600

    Oil class created

---
+++ src/Oil.cpp
@@ -0,0 +1,7 @@
+//
+// Created by pablo on 16/05/22.
+//
+
+#include "Oil.h"
+
+Oil::Oil(double price, double litres) : price(price), litres(litres) {}
---
+++ src/Oil.h
@@ -0,0 +1,21 @@
+//
+// Created by pablo on 16/05/22.
+//
+
+#ifndef STRUCTURALADAPTERDP_OIL_H
+#define STRUCTURALADAPTERDP_OIL_H
+
+
+class Oil {
+public:
+    Oil(double price, double litres);
+
+private:
+    double price;
+    double litres;
+public:
+
+};
+
+
+#endif //STRUCTURALADAPTERDP_OIL_H
